0a8ff8f588bb3ecc2748185b0a06bb6b80ebf9f5
clayton14 <claytoneasley77@gmail.com>
2024-05-30 19:12:08 -0400

    Add prompt to docker_clean.sh

--- docker_clean.sh
+++ docker_clean.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-for tag in $(sudo docker image ls | sed 1,4d | awk '{print $3}')
-do
-    sudo docker image rm -f $tag
+
+clean(){
+    for tag in $(sudo docker image ls | sed 1,4d | awk '{print $3}')
+    do
+        sudo docker image rm -f $tag
+    done
+}
+
+
+while true; do
+    read -p "Do you wish to clean up docker images [Yy/Nn]? " yn
+    case $yn in
+        [Yy]* ) clean;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
 done
+
