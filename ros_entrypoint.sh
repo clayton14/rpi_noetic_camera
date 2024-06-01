0ef169eb701d95ef27ca47b915af2408c607c375
clayton14 <claytoneasley77@gmail.com>
2024-05-31 23:03:45 -0400

    Update cmaea and ros_entrypoint
      camera.launch package is set to CAM_PKG_NAME
      ros_entrypoint sources project and runs launch file

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -2,5 +2,11 @@
 set -e
 
 # setup ros environment
-source "/opt/ros/$ROS_DISTRO/setup.bash" --
+#source "/opt/ros/$ROS_DISTRO/setup.bash" --
+chmod +x /home/catkin_ws/devel/setup.bash
+chmod +x "$CAM_PKG_PATH/src/camera_node.py"
+source /home/catkin_ws/devel/setup.bash
+#source "$CAM_PKG_PATH/src/camera_node.py"
+roslaunch "$CAM_PKG_NAME" camera.launch
+
 exec "$@"
