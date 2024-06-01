090cc6ae8f6c0308f4fc747bbd09531a7a835e86
clayton14 <claytoneasley77@gmail.com>
2024-06-01 16:39:29 -0400

    Add end of command option
      adding the -- prevents any args getting pass after it

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -3,10 +3,9 @@ set -e
 
 # setup ros environment
 #source "/opt/ros/$ROS_DISTRO/setup.bash" --
-chmod +x /home/catkin_ws/devel/setup.bash
+chmod +x "/home/catkin_ws/devel/setup.bash" --
 chmod +x "$CAM_PKG_PATH/src/camera_node.py"
 source /home/catkin_ws/devel/setup.bash
-#source "$CAM_PKG_PATH/src/camera_node.py"
 roslaunch "$CAM_PKG_NAME" camera.launch
 
 exec "$@"
